6d54d3ab2ceb7eb86c79442bc5a459e78a684b96
Zhujie2020 <1539939743@qq.com>
2020-09-21 14:22:44 +0800

    搭框

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # sy-tm
-朱杰和钱宗普的天猫
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: HomePage
+  },
+  {
+    path: '/classify',
+    component:Classfify
+  },
+  {
+    path: '/details',
+    component: Details
+  },
+  {
+    path: '/me',
+    component: Me
+  },
+  {
+    path: '/shopping',
+    component: Shopping
+  },
+
+
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
