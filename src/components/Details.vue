80b0b20a82ee98a17d757740a9c4a792baecb3df
Zhujie2020 <1539939743@qq.com>
2020-09-22 23:08:38 +0800

    改了数据

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,151 +1,161 @@
 <template>
-  <div class="container" v-if="list[id]">
-    <!-- 图片和上面的标题  -->
-    <div class="title">
-      <div class="title-top">
-        <div class="pic">
-          <img src="../assets/左白.png" />
-        </div>
-        <div class="three">
-          <div>商品</div>
-          <div class="active">评论</div>
-          <div>详情</div>
-        </div>
-        <div class="pic">
-          <img src="../assets/车白.png" />
-        </div>
-      </div>
-      <div class="photo">
-        <swiper v-if="list[id]" ref="mySwiper2" :options="swiperOptions" class="swiper-one">
-          <swiper-slide class="swiper-two" v-for="item in list[id].swiper" :key="item.id">
-            <img :src="item" alt />
-          </swiper-slide>
-        </swiper>
-        <div class="swiper-pagination"></div>
-      </div>
-    </div>
-    <!-- 价格 -->
-    <div class="price">
-      <div class="price-left" v-if="list[id]">
-        <div class="price-big">
-          ￥
-          <span class="money">{{list[id].price}}</span>
-        </div>
-        <div class="price-small">
-          <div class="noprice">
-            ￥
-            <span>{{list[id].noprice}}</span>
+  <div class="big">
+    <div class="title title-fix">
+        <div class="title-top">
+          <div class="pic">
+            <img src="../assets/左白.png" />
           </div>
-          <div class="sale">9件已售</div>
+          <div class="three">
+            <div>商品</div>
+            <div class="active">评论</div>
+            <div>详情</div>
+          </div>
+          <div class="pic">
+            <img src="../assets/车白.png" />
+          </div>
         </div>
-      </div>
-      <div class="price-right">
-        <div>距结束剩余</div>
-        <div class="time">
-          <div class="component-wrapper">{{hou}}:</div>
-          <div class="component-wrapper">{{min}}:</div>
-          <div class="component-wrapper">{{sec}}:</div>
-          <div class="component-wrapper">0{{ssec}}</div>
-        </div>
-        <div class="triangle"></div>
-      </div>
     </div>
-    <!--介绍-->
-    <div class="info">
-      <div class="info-pro" v-if="list[id]">
-        <div class="info-item">{{list[id].info}}</div>
-      </div>
-      <div class="information">
-        <div>运费:0.00</div>
-        <div>月销量100+件</div>
-        <div>广东江门</div>
-      </div>
-    </div>
-    <!-- 促销 -->
-    <div class="promotion">
-      <div class="color-gary">促销</div>
-      <div class="promotion-middle">
-        <div class="promotion-middle-top">
-          <div class="promotion-red">促销</div>
-          <div class="promotion-black">满一件，打6折</div>
-        </div>
-        <div class="promotion-middle-top">
-          <div class="promotion-red">积分</div>
-          <div class="promotion-black">购买可得15积分</div>
+    <div class="container" v-if="list[id]" >
+      <!-- 图片和上面的标题  -->
+      <div class="title">
+        <div class="photo">
+          <swiper v-if="list[id]" ref="mySwiper2" :options="swiperOptions" class="swiper-one">
+            <swiper-slide class="swiper-two" v-for="item in list[id].swiper" :key="item.id">
+              <img :src="item" alt />
+            </swiper-slide>
+          </swiper>
+          <div class="swiper-pagination"></div>
         </div>
       </div>
-      <div class="arrow-right">
-        <img src="../assets/箭头右.png" alt />
+      <!-- 价格 -->
+      <div class="price">
+        <div class="price-left" v-if="list[id]">
+          <div class="price-big">
+            ￥
+            <span class="money">{{list[id].price}}</span>
+          </div>
+          <div class="price-small">
+            <div class="noprice">
+              ￥
+              <span>{{list[id].noprice}}</span>
+            </div>
+            <div class="sale">9件已售</div>
+          </div>
+        </div>
+        <div class="price-right">
+          <div>距结束剩余</div>
+          <div class="time">
+            <div class="component-wrapper">{{hou}}:</div>
+            <div class="component-wrapper">{{min}}:</div>
+            <div class="component-wrapper">{{sec}}:</div>
+            <div class="component-wrapper">0{{ssec}}</div>
+          </div>
+          <div class="triangle"></div>
+        </div>
       </div>
-    </div>
-    <!-- 优惠 -->
-    <div class="discount">
-      <div class="calculate">
-        <img
-          src="https://img.alicdn.com/tps/i3/TB1YGS.IVXXXXchaXXXKtyaIVXX-227-63.png_320x320Q50s50.jpg_.webp"
-          alt
-        />
+      <!--介绍-->
+      <div class="info">
+        <div class="info-pro" v-if="list[id]">
+          <div class="info-item">{{list[id].info}}</div>
+        </div>
+        <div class="information">
+          <div>运费:0.00</div>
+          <div>月销量100+件</div>
+          <div>广东江门</div>
+        </div>
       </div>
-      <div class="module-scene-item">
-        <div class="service">
-          <div class="color-gary">服务</div>
-          <div>假一赔四</div>
-          <div class="dot">·</div>
-          <div>破损包退</div>
-          <div class="dot">·</div>
-          <div>赠保价险</div>
-          <div class="dot">·</div>
-          <div>退货运费险</div>
+      <!-- 促销 -->
+      <div class="promotion">
+        <div class="color-gary">促销</div>
+        <div class="promotion-middle">
+          <div class="promotion-middle-top">
+            <div class="promotion-red">促销</div>
+            <div class="promotion-black">满一件，打6折</div>
+          </div>
+          <div class="promotion-middle-top">
+            <div class="promotion-red">积分</div>
+            <div class="promotion-black">购买可得15积分</div>
+          </div>
         </div>
         <div class="arrow-right">
           <img src="../assets/箭头右.png" alt />
         </div>
       </div>
-    </div>
-    <!-- 配送 -->
-    <div class="choose">
-      <div class="choose-list">
-        <div class="color-gary">选择</div>
-        <div class="choose-desc">配送至：沧浪区，请选择颜色分类/鞋码</div>
-        <div class="arrow-right">
-          <img src="../assets/箭头右.png" alt />
+      <!-- 优惠 -->
+      <div class="discount">
+        <div class="calculate">
+          <img
+            src="https://img.alicdn.com/tps/i3/TB1YGS.IVXXXXchaXXXKtyaIVXX-227-63.png_320x320Q50s50.jpg_.webp"
+            alt
+          />
+        </div>
+        <div class="module-scene-item">
+          <div class="service">
+            <div class="color-gary">服务</div>
+            <div>假一赔四</div>
+            <div class="dot">·</div>
+            <div>破损包退</div>
+            <div class="dot">·</div>
+            <div>赠保价险</div>
+            <div class="dot">·</div>
+            <div>退货运费险</div>
+          </div>
+          <div class="arrow-right">
+            <img src="../assets/箭头右.png" alt />
+          </div>
         </div>
       </div>
-      <div class="choose-list">
-        <div class="color-gary">参数</div>
-        <div class="choose-desc">品牌 功能...</div>
-        <div class="arrow-right">
-          <img src="../assets/箭头右.png" alt />
+      <!-- 配送 -->
+      <div class="choose">
+        <div class="choose-list">
+          <div class="color-gary">选择</div>
+          <div class="choose-desc">配送至：沧浪区，请选择颜色分类/鞋码</div>
+          <div class="arrow-right">
+            <img src="../assets/箭头右.png" alt />
+          </div>
+        </div>
+        <div class="choose-list">
+          <div class="color-gary">参数</div>
+          <div class="choose-desc">品牌 功能...</div>
+          <div class="arrow-right">
+            <img src="../assets/箭头右.png" alt />
+          </div>
         </div>
       </div>
-    </div>
-    <!-- 评论 -->
-    <div class="comment">
-      <div class="comment-top">
-        <div class="shop-comment">
-          <div class="comment-left">商品评价(1033)</div>
+      <!-- 评论 -->
+      <div class="comment">
+        <div class="comment-top">
+          <div class="shop-comment">
+            <div class="comment-left">商品评价(1033)</div>
             <div class="arrow-right comment-right">
               <div class="all">查看全部</div>
               <img src="../assets/右粉.png" alt />
             </div>
           </div>
-        <div class="person">
-          <div class="touxiang"><img src="//wwc.alicdn.com/avatar/getAvatar.do?userIdStr=vGNuOHcWv88YXF-HPmvbMGx4PG8bXF--OHlHOFkSvmNSMFNuMG7eMG*eMmlIXmHS&width=40&height=40&type=sns" alt=""></div>
-          <div class="name">君**3</div>
+          <div class="person">
+            <div class="touxiang">
+              <img
+                src="//wwc.alicdn.com/avatar/getAvatar.do?userIdStr=vGNuOHcWv88YXF-HPmvbMGx4PG8bXF--OHlHOFkSvmNSMFNuMG7eMG*eMmlIXmHS&width=40&height=40&type=sns"
+                alt
+              />
+            </div>
+            <div class="name">君**3</div>
+          </div>
+          <!-- <div class="commen-self">{{list[id][comment-one]}}</div> -->
+          <div class="content">2020-09-18 颜色分类：A83149638 紫红 女；鞋码：38</div>
         </div>
-        <!-- <div class="commen-self">{{list[id][comment-one]}}</div> -->
-        <div class="content">2020-09-18 颜色分类：A83149638 紫红 女；鞋码：38</div>
-      </div>
-      <div class="comment-ask">
-         <div class="shop-comment">
-          <div class="comment-left">问大家(143)</div>
+        <div class="comment-ask">
+          <div class="shop-comment">
+            <div class="comment-left">问大家(143)</div>
             <div class="arrow-right comment-right">
               <div class="all">打开APp查看全部</div>
               <img src="../assets/右粉.png" alt />
             </div>
           </div>
+        </div>
       </div>
     </div>
+    <footer>woshihoaren</footer>
   </div>
 </template>
 
@@ -163,9 +173,9 @@ export default {
       swiperOptions: {
         pagination: {
           el: ".swiper-pagination",
-          type: "fraction",
-        }, // Some Swiper option/callback...
-      },
+          type: "fraction"
+        } // Some Swiper option/callback...
+      }
     };
   },
   methods: {
@@ -199,7 +209,7 @@ export default {
             (that.sec = "00");
         }
       }, 100);
-    },
+    }
   },
   created() {
     let that = this;
@@ -207,12 +217,12 @@ export default {
     // 为给定 ID 的 user 创建请求
     axios
       .get(url)
-      .then(function (response) {
+      .then(function(response) {
         // console.log(response);
         that.list = response.data.response;
         console.log(that.list);
       })
-      .catch(function (error) {
+      .catch(function(error) {
         console.log(error);
       });
 
@@ -224,51 +234,54 @@ export default {
     },
     id() {
       return this.$route.query.id;
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style scoped>
-.comment-ask{
+footer{
+  flex-shrink: 0;
+}
+.comment-ask {
   margin-top: 5px;
 }
-.content{
+.content {
   font-size: 10px;
   padding: 8px 0 0 0;
   border-bottom: 1px slid #ccc;
 }
-.commen-self{
+.commen-self {
   font-size: 13px;
   padding: 8px 0 0 0;
 }
-.name{
-font-size: 12px;
+.name {
+  font-size: 12px;
 }
-.touxiang img{
+.touxiang img {
   width: 100%;
 }
-.touxiang{
+.touxiang {
   margin-right: 10px;
   border-radius: 50%;
   overflow: hidden;
-  width:25px;
+  width: 25px;
   height: 25px;
 }
-.person{
+.person {
   justify-content: start;
   display: flex;
   align-items: center;
 }
-.comment-right{
-  color: #FF0036;
+.comment-right {
+  color: #ff0036;
 }
-.all{
+.all {
   margin-right: 5px;
 }
 .shop-comment {
   display: flex;
-   font-size: 14px;
+  font-size: 14px;
   /* flex-direction: column; */
   justify-content: space-between;
 }
@@ -276,7 +289,6 @@ font-size: 12px;
   display: flex;
   flex-direction: column;
   padding-bottom: 5px;
-
 }
 .comment {
   margin-top: 10px;
@@ -308,6 +320,7 @@ font-size: 12px;
 .arrow-right {
   display: flex;
   align-items: center;
+  flex-shrink: 0;
 }
 .dot {
   display: flex;
@@ -315,7 +328,8 @@ font-size: 12px;
   justify-content: center;
 }
 .service div {
-  margin-right: 8px;
+  margin-right: 3px;
+  white-space: nowrap;
 }
 .service {
   display: flex;
@@ -455,7 +469,7 @@ font-size: 12px;
 .price {
   display: flex;
   justify-content: space-between;
-  padding: 0 10px;
+  /* padding: 0 10px; */
 }
 .swiper-pagination {
   position: absolute;
@@ -486,7 +500,7 @@ font-size: 12px;
 .pic {
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: start;
   flex-shrink: 0;
 }
 .three div {
@@ -501,27 +515,43 @@ font-size: 12px;
   color: rgb(153, 153, 153);
   /* flex-shrink: 1; */
 }
+.title-top :nth-child(3){
+  justify-content: flex-end;
+}
 .title-top {
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-start;
   align-items: center;
+}
+.title-fix{
+  position: fixed;
+  height: 8%;
+  z-index: 5;
+  padding: 0;
 }
 .title {
   width: 100%;
   display: flex;
   flex-direction: column;
-  padding: 0 10px;
+  padding: 0;
   overflow: hidden;
+flex-shrink: 0;
 }
 .container {
-  overflow: auto;
-  height: 100%;
-  width: 100%;
   display: flex;
   flex-direction: column;
   background: rgb(250, 250, 250);
   /* color: rgba(153, 153, 153,0.5); */
   font-size: 1.2rem/1.5;
   color: #333;
+  overflow: auto;
+  
+}
+.big {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  /* overflow: hidden; */
 }
 </style>
