930a58813f306ff69edcc7aba355d9f942b663fd
Zhujie2020 <1539939743@qq.com>
2020-09-23 21:07:44 +0800

    写完样式 事件写一半

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Details from '../components/Details.vue'
 import HomePage from "../components/HomePage.vue"
-<<<<<<< HEAD
 import ShoppingCart from "../components/ShoppingCart.vue"
-=======
 import SearchDetail from "../components/searchDetail.vue"
 import Classify from "../components/Classify.vue"
 
->>>>>>> 102bcdf86383d6263f39fc5a44a6f15dce745b9a
 Vue.use(VueRouter)
 
 const routes = [
@@ -23,11 +20,10 @@ const routes = [
     component: HomePage
   },
   {
-<<<<<<< HEAD
     path:'/shoppingcart',
     component:ShoppingCart
-  }
-=======
+  },
+  {
     path:"/searchDetail",
     component:SearchDetail
   },
@@ -36,7 +32,6 @@ const routes = [
     component:Classify
   }
  
->>>>>>> 102bcdf86383d6263f39fc5a44a6f15dce745b9a
 
 ]
 
